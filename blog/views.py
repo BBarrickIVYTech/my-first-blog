a5c042ec22ed370c7fb505314c2b8b7f8e260e8c
BenBarrick <benbarrick8@gmail.com>
2024-12-10 14:39:12 -0500

    a

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-
+from django.contrib.auth.decorators import login_required
 
 def post_new(request):
     if request.method == "POST":
