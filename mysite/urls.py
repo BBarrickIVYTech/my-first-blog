2426d3a7afc3a0893e2f075e3e4c818bb937989d
BenBarrick <benbarrick8@gmail.com>
2024-12-10 15:21:16 -0500

    a

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,9 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
