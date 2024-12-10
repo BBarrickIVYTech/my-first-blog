b28af88004b5e19ff0590b5e5ba14c389e9462d9
BenBarrick <benbarrick8@gmail.com>
2024-12-10 14:27:03 -0500

    wa

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
 ]
